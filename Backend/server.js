10ffe7b9902c3534b1db39e1c211c66419df7c2c
Gokulsundar S <gokulsundars.cse@gmail.com>
2024-05-31 23:50:36 +0530

    update commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const path = require('path');
+const dotenv = require('dotenv');
 const app = express();
-const port = 3000;
 
+dotenv.config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+mongoose.connect(process.env.mongodb_url);
+
+const codeconfetti = mongoose.model('codeconfetti', {
+    name: String,
+    roll: String,
+    year: String,
+    section: String,
+    email: String,
+    phone: String,
+  });
+
+  const codecraft = mongoose.model('codecraft', {
+    name: String,
+    roll: String,
+    year: String,
+    section: String,
+    email: String,
+    phone: String,
+  });
+
+  const developnet = mongoose.model('developnet', {
+    teamname: String,
+    leadname: String,
+    leadroll: String,
+    leadyear: String,
+    leadsection: String,
+    leademail: String,
+    leadphone: String,
+    membername: String,
+    memberroll: String,
+    memberyear: String,
+    membersection: String,
+    memberemail: String,
+    memberphone: String,
+  });
+  
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('/', (req, res) => {
@@ -25,13 +64,24 @@ app.get('/developnet.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', '/HTML files/developnet.html'));
 });
 
-app.post('/codeconfetti', (req, res) => {
-    const formData = req.body;
-    console.log('Form Data:', formData);
-
-    res.send('<h1>Form data received</h1><p>Thank you for your submission.</p>');
+app.post('/code_confetti', (req, res) => {
+    const formdata = req.body;
+    const data = new codeconfetti(formdata);
+    data.save();
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.post('/code_craft', (req, res) => {
+    const formdata = req.body;
+    const data = new codecraft(formdata);
+    data.save();
+});
+
+app.post('/developnet', (req, res) => {
+    const formdata = req.body;
+    const data = new developnet(formdata);
+    data.save();
+});
+
+app.listen(process.env.port, () => {
+    console.log(`Server is running on http://localhost:${process.env.port}`);
 });
